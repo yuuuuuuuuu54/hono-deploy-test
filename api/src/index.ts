071179c30ebbb62d4e4832bfa8da5568c7818075
mimuyuto <mimu.yuto@icloud.com>
2025-03-31 23:53:41 +0900

    色々削除した

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,25 +2,6 @@ import { drizzle } from 'drizzle-orm/d1';
 import { Hono } from 'hono';
 import { usersSchema } from './db/schema';
 
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
-// export default {
-// 	async fetch(request, env, ctx): Promise<Response> {
-// 		return new Response('Hello World!');
-// 	},
-// } satisfies ExportedHandler<Env>;
-
 type Bindings = {
 	DB: D1Database;
 };
